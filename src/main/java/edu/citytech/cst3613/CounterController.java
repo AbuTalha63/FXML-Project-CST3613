135eae4b9154e6c682dc3c74c208aec10e03999d
Abutalha63 <usmanm6390@gmail.com>
2026-05-15 17:43:13 -0400

    Polished FXML, started on populating TreeView

--- src/main/java/edu/citytech/cst3613/CounterController.java
+++ src/main/java/edu/citytech/cst3613/CounterController.java
@@ -1,11 +1,19 @@
 package edu.citytech.cst3613;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import edu.citytech.cst3613.service.CounterService;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.layout.FlowPane;
 
-public class CounterController {
+public class CounterController implements Initializable{
 
     @FXML
     private FlowPane fpNumbers;
@@ -14,6 +22,26 @@ public class CounterController {
     private Label lblCountBy;
 
     @FXML
-    private TreeView<?> tvCounter;
+    private TreeView<String> tvCounter;
 
+    @Override
+    public void initialize(URL url, ResourceBundle resource) {
+        
+        ObservableList<Node> children = fpNumbers.getChildren(); //newer version of java u can just declare as var, thi suspports all
+
+        for (int i = 0; i < 200; i++) {
+            Label label = new Label(i + "");
+            children.add(label);
+        }
+
+        populateTreeView();
+    }
+
+    CounterService counterservice = new CounterService();
+
+    private void populateTreeView() {
+        TreeItem<String> rootItem = new TreeItem<>("Numbers");
+        var numbers = counterservice.getNumbers();
+        
+    }
 }
