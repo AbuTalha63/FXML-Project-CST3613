054e05450f61eb8fde1d45b2a1919b4d072850e9
Abutalha63 <usmanm6390@gmail.com>
2026-05-15 11:43:02 -0400

    Initial commit: JavaFX Maven project setup

---
+++ src/main/java/edu/citytech/cst3613/PrimaryController.java
@@ -0,0 +1,12 @@
+package edu.citytech.cst3613;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
---
+++ src/main/java/edu/citytech/cst3613/SearchStockController.java
@@ -0,0 +1,81 @@
+package edu.citytech.cst3613;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.FlowPane;
+
+public class SearchStockController implements Initializable {
+
+    @FXML
+    private FlowPane fpMarketCap;
+
+    @FXML
+    private FlowPane fpStocks;
+
+    @FXML
+    private FlowPane fpMonths;
+
+    @FXML
+    private Label lblRowCount;
+
+    @FXML
+    private Label lblTitle;
+
+    @FXML
+    private ToggleGroup tgMarketCap;
+
+    ObservableList<Node> stocks;
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        // Initialize controller
+        stocks = fpStocks.getChildren();
+        count(0, 1);
+
+        
+    
+    }
+    public void count(int i, int multipleOf) {
+        int start = 0;
+        for (Node node : stocks) {
+            Label label = (Label)node;
+            label.setText( (i = i + 1) * multipleOf + "" );
+        }
+    }
+
+    @FXML
+    void selectMarketCap(ActionEvent event) {
+        Node node = (Node)event.getSource();
+        String sUserData = node.getUserData().toString();
+        int multipleOf = Integer.parseInt(sUserData);
+        System.out.println(event);
+        count(0, multipleOf);
+    }
+    @FXML
+    void monthSelection(ActionEvent event) {
+        var checkBox = (CheckBox)event.getSource();
+        System.out.println(event);
+        System.out.println(checkBox.getUserData());
+
+        int monthNo = Integer.parseInt( checkBox.getUserData().toString() );
+
+        for (Node node : stocks) {
+            Label label = (Label)node;
+            int number = Integer.parseInt( label.getText().trim() );
+
+            label.getStyleClass().remove("month-color");
+            if (number % monthNo == 0 )
+                label.getStyleClass().add("month-color");
+        }
+    }
+
+}
